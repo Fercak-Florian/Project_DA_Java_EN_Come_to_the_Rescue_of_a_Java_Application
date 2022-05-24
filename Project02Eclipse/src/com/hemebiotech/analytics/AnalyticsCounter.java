82990e077aea1c58de174946958531137aa96ba4
Fercak Florian <fercak.florian@hotmail.fr>
2022-05-24 18:35:23 +0200

    Ajout du code nettoyé et fonctionnel

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,39 +5,40 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+    private static int headacheCount = 0;
+    private static int rashCount = 0;
+    private static int pupilCount = 0;
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+    public static void main(String args[]) throws Exception {
+	// first get input
+	BufferedReader reader = new BufferedReader(new FileReader(
+		"D:\\Dev\\Projet_2\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\symptoms.txt"));
+	String line = reader.readLine();
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+	int i = 0;
+	int headachesCount = 0;
+	while (line != null) {
+	    i += 1;
+	    System.out.println("symptom from file: " + line);
+	    if (line.equals("headache")) {
+		headachesCount += 1;
+		System.out.println("number of headaches: " + headachesCount);
+	    } else if (line.equals("rush")) {
+		rashCount += 1;
+	    } else if (line.contains("pupils")) {
+		pupilCount += 1;
+	    }
+
+	    line = reader.readLine(); // get another symptom
 	}
+
+	reader.close();
+
+	// next generate output
+	FileWriter writer = new FileWriter("result.out");
+	writer.write("headache: " + headacheCount + "\n");
+	writer.write("rash: " + rashCount + "\n");
+	writer.write("dialated pupils: " + pupilCount + "\n");
+	writer.close();
+    }
 }
